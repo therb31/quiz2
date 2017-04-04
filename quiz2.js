42133a6e8e0dfccd7f63b5ab5789ce9b39068b7a
Taylor Herb <tah77@pitt.edu>
2017-04-04 19:48:25 -0400

    Add files via upload

--- quiz2.js
+++ quiz2.js
@@ -1,3 +1,19 @@
-consle.log('welocme to quiz town, population...YOU!')
 
+$(document).ready(function(){
+  $("#dialog").click(function(){
+    $("#dialog1").dialog({
+      modal:true
+    });
+  });
+});
+
+/*console log usually used for debugging purposes..not sure how else to use stylistically so I added
+a different JS function
+
+$(document).ready(function(){
+console.log('welocme to quiz town, population...YOU!'){
+};
+};
+
+moved transition for dynamic to css file
 @('.dynamic').hmtl('<div p>HI THERE, I\'M FROM JAVASCRIPT</p></div>').fadeIn('slo')
